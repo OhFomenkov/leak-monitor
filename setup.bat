72a50ed0940c0d36ce452a9d2fe6d58ba6b35ca1
OhFomenkov <151652496+OhFomenkov@users.noreply.github.com>
2026-06-07 20:18:49 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,14 +1,26 @@
 @echo off
 chcp 65001 >nul
-echo [*] Активация виртуального окружения...
+
+echo [*] Проверка виртуального окружения...
+if not exist venv (
+    echo [*] Окружение не найдено. Создаю...
+    python -m venv venv
+)
+
+echo [*] Активация окружения...
 call venv\Scripts\activate.bat
 
-echo [*] Инициализация базы данных...
+echo [*] Установка библиотек...
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo [*] Инициализация структуры базы данных...
 python -m app.database.db_init
 
-echo [*] Наполнение базы демо-данными...
+echo [*] Наполнение демо-данными...
 python scripts/db_seed.py
 
 echo.
-echo [!] Подготовка завершена! Теперь можно запускать систему.
+echo [!] Установка завершена успешно!
+echo [!] Теперь вы можете запустить проект через start.bat
 pause
